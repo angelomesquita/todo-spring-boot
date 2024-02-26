cf07c136706a1ddc812c110e2f25f214d759b586
Angelo Mesquita <angeloamesquita@gmail.com>
2024-02-26 20:35:23 -0300

    WIP

--- src/main/java/net/atos/acelerajava/todo/model/Task.java
+++ src/main/java/net/atos/acelerajava/todo/model/Task.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
 @Entity
+@Data
 public class Task {
 
     @Id
--- src/main/java/net/atos/acelerajava/todo/repository/TaskRepository.java
+++ src/main/java/net/atos/acelerajava/todo/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package net.atos.acelerajava.todo.repository;
 
 import net.atos.acelerajava.todo.model.Task;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
