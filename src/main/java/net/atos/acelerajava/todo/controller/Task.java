1b1d8f92be1891638692a8e0f4500585b0192880
Angelo Mesquita <angeloamesquita@gmail.com>
2024-02-26 19:09:15 -0300

    WIP

---
+++ src/main/java/net/atos/acelerajava/todo/controller/Task.java
@@ -0,0 +1,19 @@
+package net.atos.acelerajava.todo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Task {
+
+    // Autowired
+
+    // GetMapping
+
+    // GetMapping com ID
+
+    // PostMapping
+
+    // PutMapping
+
+    // DeleteMapping
+}
---
+++ src/main/java/net/atos/acelerajava/todo/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package net.atos.acelerajava.todo.repository;
+
+import net.atos.acelerajava.todo.model.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+}
